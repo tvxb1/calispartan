e927d6a41bcf3ce8b8a069add48b7a99d6d54ac0
Théo Fabry <theo.fabry@gmail.com>
2023-12-24 00:17:16 +0100

    Added workouts

--- src/Workouts.js
+++ src/Workouts.js
@@ -61,6 +61,133 @@ const workouts = [{
             "10 dips and 10 situps",
         ]
     },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "5 rounds RIR 1 min 30 s",
+            "10 pushups",
+            "5 pullups",
+            "10 squat jumps",
+            "200 m run",
+            "200 m sprint",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "15 rounds RIR 30 s",
+            "1 min jump rope",
+            "20 mountain climbers",
+            "5 squat jumps",
+            "5 burpee-tractions",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "short",
+        directions: [
+            "AMRAP 20 min without RIR",
+            "10 pushups",
+            "3 pullups",
+            "5 dips",
+            "100 m run",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "6 rounds RIR 1 min 30 s",
+            "15 jumping jacks",
+            "10 pushups",
+            "10 burpees",
+            "5 pullups",
+            "10 grouped jumps",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "short",
+        directions: [
+            "6 rounds of 3 min each RIR 1 min, AMRAP in each round",
+            "12 knee raises",
+            "10 pushups",
+            "10 situps",
+            "3 pullups",
+            "10 squats",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "8 rounds RIR 1 min",
+            "400 m run",
+            "15 rowing crunches",
+            "15 pushups",
+            "5 pullups",
+            "10 situps",
+            "10 jmping jacks",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "short",
+        directions: [
+            "EMOM 20 minutes (1 round per minute)",
+            "5 pullups",
+            "10 burpees",
+            "10 pushups",
+            "20 squats",
+            "10 ab rolls",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "short",
+        directions: [
+            "2 rounds of 10 min RIR 3 min, AMRAP in each round",
+            "5 pushups",
+            "3 pullups",
+            "5 dips",
+            "5 situps",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "20 rounds withour RIR",
+            "50 m run",
+            "5 burpee tractions",
+            "50 m run",
+            "5 toes-to-bar",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "5 rounds without RIR",
+            "5 jumping lunges",
+            "10 s sprint on the spot",
+            "5 pullups",
+            "5 forward lunges",
+            "200 m run",
+        ]
+    },
 ]
 
 export default workouts
