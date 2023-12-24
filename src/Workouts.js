9ce0281b8d1866ad4a22b05e3b5bbc9b9b9ae002
Théo Fabry <theo.fabry@gmail.com>
2023-12-24 13:48:12 +0100

    Added workouts

--- src/Workouts.js
+++ src/Workouts.js
@@ -168,7 +168,7 @@ const workouts = [{
         bodyPart: "full",
         duration: "medium",
         directions: [
-            "20 rounds withour RIR",
+            "20 rounds without RIR",
             "50 m run",
             "5 burpee tractions",
             "50 m run",
@@ -181,11 +181,150 @@ const workouts = [{
         duration: "medium",
         directions: [
             "5 rounds without RIR",
-            "5 jumping lunges",
+            "6 jumping lunges",
+            "10 pushups",
             "10 s sprint on the spot",
             "5 pullups",
             "5 forward lunges",
             "200 m run",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "5 rounds without RIR",
+            "15 squats",
+            "10 pushups",
+            "5 forward lunges",
+            "5 pullups",
+            "5 forward lunges",
+            "400 m run",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "6 rounds RIR 30 s",
+            "100 m run",
+            "10 jumping lunges",
+            "10 pushups",
+            "100 m run",
+            "20 diagonal bridges",
+            "5 pullups",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "AMRAP 30m",
+            "5 burpees",
+            "10 situps",
+            "5 pullups",
+            "10 bridges",
+            "10 pushups",
+            "50 m sprint",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "long",
+        directions: [
+            "4000 m run and every 400 m",
+            "12 pushups",
+            "10 crunches",
+            "5 pullups",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "long",
+        directions: [
+            "8 rounds RIR 1 min",
+            "300 m run",
+            "10 candle crunches",
+            "10 pushups",
+            "5 pullups",
+            "5 toes to bar",
+            "5 burpee pullups",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "5 rounds RIR 1 min 30 s",
+            "300 m run",
+            "12 pushups",
+            "20 heels to buttocks",
+            "12 pushups",
+            "300 m run",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "2000 m run and every 200 m",
+            "5 burpee tractions",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "AMRAP 25 min",
+            "10 pushups",
+            "10 jumping jacks",
+            "8 toes to bar",
+            "200 m run",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "long",
+        directions: [
+            "3000 m run and every 200 m",
+            "10 pushups",
+            "5 pullups",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "long",
+        directions: [
+            "10 rounds RIR 1 min",
+            "10 pushups",
+            "5 burpee-pullups",
+            "10 squat jumps",
+            "100 m run",
+            "100 m sprint",
+        ]
+    },
+    {
+        difficulty: "easy",
+        bodyPart: "full",
+        duration: "medium",
+        directions: [
+            "1 round",
+            "5 pushups, 3 pullups, 2 dips, 5 bridges rest 1 min",
+            "6 pushups, 4 pullups, 3 dips, 6 bridges rest 1 min",
+            "7 pushups, 5 pullups, 4 dips, 7 bridges rest 1 min",
+            "8 pushups, 6 pullups, 5 dips, 8 bridges rest 1 min",
+            "9 pushups, 7 pullups, 6 dips, 9 bridges rest 1 min",
+            "10 pushups, 8 pullups, 7 dips, 10 bridges rest 1 min",
         ]
     },
 ]
